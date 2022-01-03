42b519e80a5ff7a9275c7c7eeaae2501b33cb8d1
Nguyen Van Suc <nguyensuc7699@gmail.com>
2022-01-03 13:05:29 +0700

    fix start

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const io = require('socket.io')(process.env.PORT||"https://shop-socket.herokuapp.com/", {
+const io = require('socket.io')(process.env.PORT||5000, {
     cors: {
       origin: 'https://shop-frontend-seven.vercel.app',
     },
